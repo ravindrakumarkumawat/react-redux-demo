e8438d32ba384f27407b82e4e32070652d5d96c4
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 15:43:55 +0530

    state changed

---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyIceCream } from '../redux'
+
+function IceCreamContainer(props) {
+  return (
+    <div>
+      <h1>Redux</h1>
+      <h2>Number of IceCream - {props.numOfIceCreams}</h2>
+      <button onClick={props.buyIceCream}>Buy IceCream</button>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfIceCreams: state.iceCream.numOfIceCreams
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyIceCream: () => dispatch(buyIceCream())
+  }
+}
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(IceCreamContainer)
+
