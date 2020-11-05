e9b1a8240af7a562e7193af527c13a3a78287704
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 14:21:38 +0530

    useSelector Hook is added that works like mapStatetoProps function

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function HooksCakeContainer() {
+  const numOfCakes = useSelector(state => state.numOfCakes)
   return (
     <div>
       <h1>Hooks</h1>
-      <h2>Number of cakes -</h2>
+      <h2>Number of cakes - {numOfCakes}</h2>
       <button>Buy Cake</button>
     </div>
   )
