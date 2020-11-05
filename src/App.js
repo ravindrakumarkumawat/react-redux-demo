c0879dfd5d872c0ddbee58cdf9b94bdd59cde07a
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 17:08:17 +0530

    action payload is added now user can enter the number

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css'
 import CakeContainer from './components/CakeContainer'
+import NewCakeContainer from './components/NewCakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 
@@ -13,6 +14,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
