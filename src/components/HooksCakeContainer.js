c10454d2afa9a2c3aaada90adde48c70c69afde3
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 14:37:16 +0530

    dispatch hook is added from react-redux

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux'
 
 function HooksCakeContainer() {
   const numOfCakes = useSelector(state => state.numOfCakes)
+  const dispatch = useDispatch()
   return (
     <div>
       <h1>Hooks</h1>
       <h2>Number of cakes - {numOfCakes}</h2>
-      <button>Buy Cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
     </div>
   )
 }
