f64c5dc0c645842191893bc345d85dd584ee9caa
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 18:41:46 +0530

    redux thunk is added for async action

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import reduxThunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, reduxThunk)))
 
 export default store
