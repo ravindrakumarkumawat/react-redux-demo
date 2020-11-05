d841072bc64f09fc82c42cc6eba6322e58f85417
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-05 18:36:47 +0530

    user reducer is added to root reducer

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -10,7 +10,7 @@ const initialState = {
   error: ''
 }
 
-const reducer = (state = initialState, action) => {
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USERS_REQUEST: return {
       ...state,
@@ -30,4 +30,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default userReducer
